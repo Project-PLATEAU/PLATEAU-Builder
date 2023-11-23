04b33e45399197e433d28eda840fd2ec9eea733c
kei-w <k4h.wwwww@gmail.com>
2023-11-23 23:32:09 +0900

    Add GltfExporter class for exporting CityModel

---
+++ src/main/java/org/plateau/citygmleditor/exporters/GltfExporter.java
@@ -0,0 +1,10 @@
+package org.plateau.citygmleditor.exporters;
+
+import org.plateau.citygmleditor.citymodel.CityModel;
+import org.plateau.citygmleditor.citymodel.geometry.ILODSolid;
+
+public class GltfExporter {
+    public static void export(String fileUrl, CityModel cityModel, ILODSolid lodSolid) {
+        
+    }
+}
