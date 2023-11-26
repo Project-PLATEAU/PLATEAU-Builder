946c6ba23542cfab5456b3a94a0f764918fe70be
Reee009876 <113989589+Reee009876@users.noreply.github.com>
2023-11-26 18:04:35 +0900

    Revert "CityGMLのエクスポート対応"
    This reverts commit fcfc564bbf70c84e81b476afdb4a3b2f55bc749e.

--- src/main/java/org/plateau/citygmleditor/citygmleditor/MainController.java
+++ src/main/java/org/plateau/citygmleditor/citygmleditor/MainController.java
@@ -36,15 +36,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import java.awt.FileDialog;
-import java.awt.Frame;
-import java.awt.event.FocusEvent;
 
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
@@ -57,7 +49,6 @@ import org.citygml4j.model.citygml.ade.ADEException;
 import org.citygml4j.xml.io.writer.CityGMLWriteException;
 import org.plateau.citygmleditor.citymodel.CityModel;
 import org.plateau.citygmleditor.exporters.GmlExporter;
-import org.plateau.citygmleditor.exporters.TextureExporter;
 import org.plateau.citygmleditor.importers.gml.GmlImporter;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -79,29 +70,13 @@ import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
 import javafx.stage.FileChooser;
 import javafx.util.Duration;
-
 import org.plateau.citygmleditor.importers.Importer3D;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.FieldPosition;
+
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.text.StyledEditorKit;
-
-import javafx.stage.DirectoryChooser;
 import javafx.geometry.BoundingBox;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.ButtonType;
-import java.nio.file.*;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 
 /**
  * Controller class for main fxml file.
@@ -123,8 +98,6 @@ public class MainController implements Initializable {
     private String loadedURL;
     private String[] supportedFormatRegex;
     private SessionManager sessionManager = SessionManager.getSessionManager();
-    private String sourceRootDirPath;
-    private String[] importGmlPathComponents;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -227,8 +200,6 @@ public class MainController implements Initializable {
 
     private void doLoadGml(String fileUrl) {
         loadedURL = fileUrl;
-        importGmlPathComponents = fileUrl.split("\\\\");
-        sourceRootDirPath = new File(new File(new File(loadedURL).getParent()).getParent()).getParent();
         sessionManager.getProperties().setProperty(CityGMLEditorApp.FILE_URL_PROPERTY, fileUrl);
         try {
             var root = GmlImporter.loadGml(fileUrl);
@@ -345,103 +316,26 @@ public class MainController implements Initializable {
     }
 
     public void export(ActionEvent event) {
-
-        String[] destRootDirComponents = importGmlPathComponents[importGmlPathComponents.length - 4].split("_");// インポートしたcitygmlのルートフォルダネームを_で分解
-        String cityCode = destRootDirComponents[0];
-        String cityName = destRootDirComponents[1];
-        String developmentYear = destRootDirComponents[2];
-        String updateCount = Integer.toString(Integer.parseInt(destRootDirComponents[4]) + 1);
-        String options;
-        String rootDirName;// エクスポート先のルートフォルダの名前
-        String udxDirName = "udx";
-        String bldgDirName = "bldg";
-
-        // ダイアログで表示される初期のフォルダ名を指定
-        String defaultDirName = "";
-        if (destRootDirComponents[destRootDirComponents.length - 1].equals("op")) {
-            if (destRootDirComponents.length == 7) {
-                options = destRootDirComponents[5];
-                defaultDirName = cityCode + "_" + cityName + "_" + developmentYear + "_" + "citygml" + "_" +
-                        updateCount + "_" + options + "_" + "op";
-            } else {
-                defaultDirName = cityCode + "_" + cityName + "_" + developmentYear + "_" + "citygml" + "_" +
-                        updateCount + "_" + "op";
-            }
-        } else {
-            if (destRootDirComponents.length == 6) {
-                options = destRootDirComponents[5];
-                defaultDirName = cityCode + "_" + cityName + "_" + developmentYear + "_" + "citygml" + "_" +
-                        updateCount + "_" + options;
-            } else {
-                defaultDirName = cityCode + "_" + cityName + "_" + developmentYear + "_" + "citygml" + "_" +
-                        updateCount;
-            }
-        }
-        // テキスト入力ダイアログを表示し、ユーザーにフォルダ名を入力させる(仮のフォルダ名は表示)
-        TextInputDialog dialog = new TextInputDialog(defaultDirName);
-        dialog.setTitle("Input Root Folder Name");
-        dialog.setHeaderText(null);
-        dialog.setContentText("Folder Name:");
-
-        Optional<String> result = dialog.showAndWait();
-        rootDirName = dialog.getResult();
-
-        DirectoryChooser directoryChooser = new DirectoryChooser();
+        FileChooser chooser = new FileChooser();
         if (loadedPath != null) {
-            directoryChooser.setInitialDirectory(loadedPath.getAbsoluteFile().getParentFile());// 初期ディレクトリ指定
+            chooser.setInitialDirectory(loadedPath.getAbsoluteFile().getParentFile());
         }
-        // 初期ディレクトリを設定（オプション）
-        // directoryChooser.setInitialDirectory(new File("/path/to/initial/directory"));
-        directoryChooser.setTitle("Export CityGML");
-        var selectedDirectory = directoryChooser.showDialog(null);
-
-        // インポート元からエクスポート先のフォルダへコピー
-        try {
-            folderCopy(Paths.get(sourceRootDirPath),
-                    Paths.get(selectedDirectory.getAbsolutePath() + "/" + rootDirName + "/"));
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("CityGML", "*.gml"));
+        chooser.setTitle("Export CityGML");
+        File newFile = chooser.showSaveDialog(openMenuBtn.getScene().getWindow());
 
         var content = (Group) CityGMLEditorApp.getUIManager().getContent();
         var cityModelNode = content.getChildren().get(0);
-
         if (cityModelNode == null)
             return;
 
         var cityModel = (CityModel) cityModelNode;
+
         try {
-            // CityGMLのエクスポート
-            GmlExporter.export(
-                    Paths.get(selectedDirectory.getAbsolutePath() + "/" + rootDirName + "/" +
-                            udxDirName + "/"
-                            + bldgDirName
-                            + "/" + importGmlPathComponents[importGmlPathComponents.length - 1])
-                            .toString(),
-                    cityModel.getGmlObject(),
-                    cityModel.getSchemaHandler());
-            // Appearanceのエクスポート
-            TextureExporter.export(
-                    selectedDirectory.getAbsolutePath() + "/" + rootDirName + "/" + udxDirName + "/" + bldgDirName,
-                    cityModel);
+            GmlExporter.export(newFile.toString(), cityModel.getGmlObject(), cityModel.getSchemaHandler());
         } catch (ADEException | CityGMLWriteException | CityGMLBuilderException e) {
             throw new RuntimeException(e);
         }
-
     }
-
-    // フォルダコピーメソッド(udx以下は無視)
-    public void folderCopy(Path sourcePath, Path destinationPath) throws IOException {
-        String skipPattern = sourceRootDirPath.replace("\\", "\\\\") + "\\\\udx\\\\.*";
-        Files.walk(sourcePath).forEach(path -> {
-            if (!path.toString().matches(skipPattern)) {
-                try {
-                    Files.copy(path, destinationPath.resolve(sourcePath.relativize(path)));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
 }
