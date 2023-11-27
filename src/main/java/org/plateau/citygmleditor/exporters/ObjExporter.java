79196060969e402160fef16ebc4a9fef3cd30b95
kei-w <k4h.wwwww@gmail.com>
2023-11-28 01:56:58 +0900

    LOD2Solid export implemented

--- src/main/java/org/plateau/citygmleditor/exporters/ObjExporter.java
+++ src/main/java/org/plateau/citygmleditor/exporters/ObjExporter.java
@@ -4,26 +4,38 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
-import org.citygml4j.model.gml.geometry.primitives.Envelope;
-import org.plateau.citygmleditor.citymodel.Building;
 import org.plateau.citygmleditor.citymodel.CityModel;
 import org.plateau.citygmleditor.citymodel.geometry.ILODSolid;
 import org.plateau.citygmleditor.citymodel.geometry.LOD2Solid;
 
-public class ObjExporter {
-    public static void export(String fileUrl, CityModel cityModel, Building building, ILODSolid lodSolid) {
-        Envelope envelope = building.getEnvelope();
+import javafx.scene.paint.PhongMaterial;
 
+public class ObjExporter {
+    public static void export(String fileUrl, CityModel cityModel, ILODSolid lodSolid) {
         // TODO:仮実装
         if (lodSolid instanceof LOD2Solid) {
             LOD2Solid lod2Solid = (LOD2Solid) lodSolid;
             var polygons = lod2Solid.getPolygons();
 
             // TODO:GeometryFactoryからパクってきた。共通化したい
+            String materialUrl = null;
             var indexCount = 0;
             for (var polygon : polygons) {
                 indexCount += polygon.getFaces().length;
+                if (materialUrl == null) {
+                    var surfaceData = polygon.getSurfaceData();
+                    if (surfaceData != null) {
+                        var material = surfaceData.getMaterial();
+                        if (material instanceof PhongMaterial) {
+                            PhongMaterial phongMaterial = (PhongMaterial) material;
+                            materialUrl = phongMaterial.getDiffuseMap().getUrl();
+                        }
+                    }
+                }
             }
 
             var faces = new int[indexCount];
@@ -61,26 +73,17 @@ public class ObjExporter {
                     // x
                     uvs[uvIndexOffset + i] = (float) subUVs[i];
                     // y
-                    uvs[uvIndexOffset + i + 1] = 1 - (float) subUVs[i + 1];
+                    uvs[uvIndexOffset + i + 1] = (float) subUVs[i + 1];
                 }
                 uvIndexOffset += subUVs.length;
             }
 
-            // TODO: たぶん座標変換しないといけない
-
             File file = new File(fileUrl);
-            try (var stream = new FileOutputStream(file);
+            var fileName = file.getName();
+            var mtlFileName = String.format("%s.mtl", fileName.substring(0, fileName.lastIndexOf(".")));
+            try (var stream = new FileOutputStream(file, false);
                     var writer = new OutputStreamWriter(stream, "UTF-8")) {
-
-                var origin = envelope.getLowerCorner().toList3d();
-                var lowerCorner = envelope.getLowerCorner().toList3d();
-                var upperCorner = envelope.getUpperCorner().toList3d();
-                var fileName = file.getName();
-                int dotIndex = fileName.lastIndexOf(".");
-                writer.write(String.format("# Origin: %f %f %f\r\n", origin.get(0), origin.get(1), origin.get(2)));
-                writer.write(String.format("# Lower : %f %f %f\r\n", lowerCorner.get(0), lowerCorner.get(1), lowerCorner.get(2)));
-                writer.write(String.format("# Upper : %f %f %f\r\n", upperCorner.get(0), upperCorner.get(1), upperCorner.get(2)));
-                writer.write(String.format("# mtllib %s.mtl", fileName.substring(0, dotIndex)));
+                writer.write(String.format("mtllib %s\r\n", mtlFileName));
                 writer.write("g model\r\n");
 
                 for (int i = 0; i < vertices.length; i += 3) {
@@ -89,15 +92,46 @@ public class ObjExporter {
                 for (int i = 0; i < uvs.length; i += 2) {
                     writer.write(String.format("vt %f %f\r\n", uvs[i], uvs[i + 1]));
                 }
+                writer.write("usemtl Material\r\n");
                 for (int i = 0; i < faces.length; i += 6) {
-                    writer.write(String.format("f %d/%d %d/%d %d/%d\r\n", faces[i] + 1, faces[i + 1] + 1, faces[i + 2] + 1, faces[i + 3] + 1, faces[i + 4] + 1, faces[i + 5] + 1));
+                    writer.write(String.format("f %d/%d %d/%d %d/%d\r\n", faces[i] + 1, faces[i + 1] + 1,
+                            faces[i + 2] + 1, faces[i + 3] + 1, faces[i + 4] + 1, faces[i + 5] + 1));
                 }
 
-                // TODO:テクスチャのエクスポート
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try (var stream = new FileOutputStream(new File(file.getParent(), mtlFileName), false);
+                    var writer = new OutputStreamWriter(stream, "UTF-8")) {
+                writer.write("newmtl Material\r\n");
+                writer.write("Ka 0.000000 0.000000 0.000000\r\n");
+                if (materialUrl != null) {
+                    writer.write("Kd 1.0 1.0 1.0\r\n");
+                } else {
+                    writer.write("Kd 0.45 0.5 0.5\r\n");
+                }
+                writer.write("Ks 0.000000 0.000000 0.000000\r\n");
+                writer.write("Ns 2.000000\r\n");
+                writer.write("d 1.000000\r\n");
+                writer.write("Tr 0.000000\r\n");
+                writer.write("Pr 0.333333\r\n");
+                writer.write("Pm 0.080000\r\n");
+                if (materialUrl != null) {
+                    File materialFile = new File(materialUrl);
+                    var materialFileName = materialFile.getName();
+                    writer.write(String.format("map_Kd %s\r\n", materialFileName));
+
+                    file.getParent();
+                    var copyPath = new File(file.getParent(), materialFileName);
+                    Files.copy(Paths.get(materialFile.getAbsolutePath()), Paths.get(copyPath.getAbsolutePath()),
+                            StandardCopyOption.REPLACE_EXISTING);
+                }
+                writer.flush();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-
 }
