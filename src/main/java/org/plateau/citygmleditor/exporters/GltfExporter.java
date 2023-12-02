168d3b5648ff7e6d09a52af998a775f9b96f29f5
kei-w <k4h.wwwww@gmail.com>
2023-12-02 16:35:08 +0900

    Separate node for each boundary

--- src/main/java/org/plateau/citygmleditor/exporters/GltfExporter.java
+++ src/main/java/org/plateau/citygmleditor/exporters/GltfExporter.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.plateau.citygmleditor.citymodel.CityModel;
+import org.plateau.citygmleditor.citymodel.geometry.BoundarySurface;
 import org.plateau.citygmleditor.citymodel.geometry.ILODSolid;
 import org.plateau.citygmleditor.citymodel.geometry.LOD2Solid;
 
@@ -27,89 +30,85 @@ import de.javagl.jgltf.model.v2.MaterialModelV2;
 import javafx.scene.paint.PhongMaterial;
 
 public class GltfExporter {
+    private static final MaterialModelV2 defaultMaterialModel;
+
+    static {
+        MaterialBuilder materialBuilder = MaterialBuilder.create();
+        materialBuilder.setBaseColorFactor(0.9f, 0.9f, 0.9f, 1.0f);
+        materialBuilder.setDoubleSided(true);
+        defaultMaterialModel = materialBuilder.build();
+    }
+
     public static void export(String fileUrl, CityModel cityModel, ILODSolid lodSolid) {
-        // TODO:仮実装
         if (lodSolid instanceof LOD2Solid) {
             LOD2Solid lod2Solid = (LOD2Solid) lodSolid;
 
-            DefaultMeshModel meshModel = new DefaultMeshModel();
+            DefaultSceneModel sceneModel = new DefaultSceneModel();
+            Map<String, MaterialModelV2> materialMap = new HashMap<>();
 
-            MaterialBuilder materialBuilder = MaterialBuilder.create();
-            materialBuilder.setBaseColorFactor(0.9f, 0.9f, 0.9f, 1.0f);
-            materialBuilder.setDoubleSided(true);
-            MaterialModelV2 defaultMaterialModel = materialBuilder.build();
-            MaterialModelV2 textureMaterialModel = null;
+            for (var boundary : lod2Solid.getBoundaries()) {
+                DefaultNodeModel nodeModel = new DefaultNodeModel();
+                nodeModel.setName(boundary.getId());
 
-            var polygons = lod2Solid.getPolygons();
-            for (var polygon : polygons) {
-                var polygonFaces = polygon.getFaces();
-                var faces = new int[polygonFaces.length / 2];
-                for (var i = 0; i < faces.length; i += 3) {
-                    faces[i] = polygonFaces[i * 2];
-                    faces[i + 1] = polygonFaces[i * 2 + 2];
-                    faces[i + 2] = polygonFaces[i * 2 + 4];
-                }
+                for (var polygon : boundary.getPolygons()) {
+                    DefaultMeshModel meshModel = new DefaultMeshModel();
 
-                // 右手系Y-up
-                var subVertices = polygon.getAllVertices();
-                float positions[] = new float[subVertices.length];
-                for (int i = 0; i < subVertices.length; i += 3) {
-                    positions[i] = (float) subVertices[i + 1];
-                    positions[i + 1] = (float) subVertices[i + 2];
-                    positions[i + 2] = (float) subVertices[i + 0];
-                }
-
-                var subUVs = polygon.getAllUVs();
-                var uvs = new float[subUVs.length];
-                for (int i = 0; i < subUVs.length; i += 2) {
-                    uvs[i] = (float) subUVs[i];
-                    uvs[i + 1] = 1 - (float) subUVs[i + 1];
-                }
-
-                MeshPrimitiveBuilder meshPrimitiveBuilder = MeshPrimitiveBuilder.create();
-                meshPrimitiveBuilder.setIntIndicesAsShort(IntBuffer.wrap(faces));
-                meshPrimitiveBuilder.addPositions3D(FloatBuffer.wrap(positions));
-                meshPrimitiveBuilder.addTexCoords02D(FloatBuffer.wrap(uvs));
-
-                DefaultMeshPrimitiveModel meshPrimitiveModel = meshPrimitiveBuilder.build();
-                MaterialModelV2 materialModel = null;
-
-                var surfaceData = polygon.getSurfaceData();
-                if (surfaceData != null) {
-                    var material = surfaceData.getMaterial();
-                    if (material instanceof PhongMaterial) {
-                        if (textureMaterialModel == null) {
-                            PhongMaterial phongMaterial = (PhongMaterial) material;
-                            var materialFile = new File(phongMaterial.getDiffuseMap().getUrl());
-                            var inputUri = Paths.get(materialFile.getAbsolutePath()).toUri().normalize().toString();
-                            DefaultImageModel imageModel = ImageModels.create(inputUri, materialFile.getName());
-                            var textureModel = new DefaultTextureModel();
-                            textureModel.setImageModel(imageModel);
-                            materialBuilder.setBaseColorTexture(textureModel, 0);
-                            textureMaterialModel = materialBuilder.build();
-                        }
-                        materialModel = textureMaterialModel;
+                    var polygonFaces = polygon.getFaces();
+                    var faces = new int[polygonFaces.length / 2];
+                    for (var i = 0; i < faces.length; i += 3) {
+                        faces[i] = polygonFaces[i * 2];
+                        faces[i + 1] = polygonFaces[i * 2 + 2];
+                        faces[i + 2] = polygonFaces[i * 2 + 4];
                     }
-                } else {
-                    materialModel = defaultMaterialModel;
-                }
 
-                meshPrimitiveModel.setMaterialModel(materialModel);
-                meshModel.addMeshPrimitiveModel(meshPrimitiveModel);
+                    // 右手系Y-up
+                    var subVertices = polygon.getAllVertices();
+                    float positions[] = new float[subVertices.length];
+                    for (int i = 0; i < subVertices.length; i += 3) {
+                        positions[i] = (float) subVertices[i + 1];
+                        positions[i + 1] = (float) subVertices[i + 2];
+                        positions[i + 2] = (float) subVertices[i + 0];
+                    }
+
+                    var subUVs = polygon.getAllUVs();
+                    var uvs = new float[subUVs.length];
+                    for (int i = 0; i < subUVs.length; i += 2) {
+                        uvs[i] = (float) subUVs[i];
+                        uvs[i + 1] = 1 - (float) subUVs[i + 1];
+                    }
+
+                    MeshPrimitiveBuilder meshPrimitiveBuilder = MeshPrimitiveBuilder.create();
+                    meshPrimitiveBuilder.setIntIndicesAsShort(IntBuffer.wrap(faces));
+                    meshPrimitiveBuilder.addPositions3D(FloatBuffer.wrap(positions));
+                    meshPrimitiveBuilder.addTexCoords02D(FloatBuffer.wrap(uvs));
+
+                    DefaultMeshPrimitiveModel meshPrimitiveModel = meshPrimitiveBuilder.build();
+                    MaterialModelV2 materialModel = null;
+
+                    var surfaceData = polygon.getSurfaceData();
+                    if (surfaceData != null) {
+                        var material = surfaceData.getMaterial();
+                        if (material instanceof PhongMaterial) {
+                            PhongMaterial phongMaterial = (PhongMaterial) material;
+                            var url = phongMaterial.getDiffuseMap().getUrl();
+                            if (materialMap.containsKey(url)) {
+                                materialModel = materialMap.get(url);
+                            } else {
+                                materialModel = createMaterial(phongMaterial);
+                                materialMap.put(url, materialModel);
+                            }
+                        }
+                    } else {
+                        materialModel = defaultMaterialModel;
+                    }
+
+                    meshPrimitiveModel.setMaterialModel(materialModel);
+                    meshModel.addMeshPrimitiveModel(meshPrimitiveModel);
+                    nodeModel.addMeshModel(meshModel);
+                }
+                sceneModel.addNode(nodeModel);
             }
 
-            // Create a node with the mesh
-            DefaultNodeModel nodeModel = new DefaultNodeModel();
-            nodeModel.addMeshModel(meshModel);
-
-            // Create a scene with the node
-            DefaultSceneModel sceneModel = new DefaultSceneModel();
-            sceneModel.addNode(nodeModel);
-
-            // Pass the scene to the model builder. It will take care
-            // of the other model elements that are contained in the scene.
-            // (I.e. the mesh primitive and its accessors, and the material
-            // and its textures)
             GltfModelBuilder gltfModelBuilder = GltfModelBuilder.create();
             gltfModelBuilder.addSceneModel(sceneModel);
             DefaultGltfModel gltfModel = gltfModelBuilder.build();
@@ -135,5 +134,20 @@ public class GltfExporter {
                 }
             }
         }
+    }
+
+    private static MaterialModelV2 createMaterial(PhongMaterial material) {
+        var materialFile = new File(material.getDiffuseMap().getUrl());
+        var inputUri = Paths.get(materialFile.getAbsolutePath()).toUri().normalize().toString();
+        DefaultImageModel imageModel = ImageModels.create(inputUri, materialFile.getName());
+        var textureModel = new DefaultTextureModel();
+        textureModel.setImageModel(imageModel);
+
+        MaterialBuilder materialBuilder = MaterialBuilder.create();
+        materialBuilder.setBaseColorFactor(0.9f, 0.9f, 0.9f, 1.0f);
+        materialBuilder.setDoubleSided(true);
+        materialBuilder.setBaseColorTexture(textureModel, 0);
+
+        return materialBuilder.build();
     }
 }
