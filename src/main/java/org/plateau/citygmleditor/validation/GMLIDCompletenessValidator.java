903eaa4e2ab7110c1f90f50a971cea9117879284
holyhung0112 <giusehung1998@gmail.com>
2024-01-17 09:23:17 +0700

    showing duplicate gmlID

--- src/main/java/org/plateau/citygmleditor/validation/GMLIDCompletenessValidator.java
+++ src/main/java/org/plateau/citygmleditor/validation/GMLIDCompletenessValidator.java
@@ -1,11 +1,11 @@
 package org.plateau.citygmleditor.validation;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.plateau.citygmleditor.citymodel.CityModelView;
 import org.plateau.citygmleditor.constant.TagName;
 import org.plateau.citygmleditor.utils.XmlUtil;
 import org.plateau.citygmleditor.world.World;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
@@ -16,48 +16,41 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GMLIDCompletenessValidator implements IValidator {
-    public List<ValidationResultMessage> validate(CityModelView cityModel) throws ParserConfigurationException, IOException, SAXException {
-        Set<String> gmlIDs = new HashSet<>();
-        List<ValidationResultMessage> messages = new ArrayList<>();
-
-        messages.add(new ValidationResultMessage(
-                ValidationResultMessageType.Info,
-                "gml:idの完全性を検証中..."
-        ));
-
+    public List<ValidationResultMessage> validate(CityModelView cityModelView) throws ParserConfigurationException, IOException, SAXException {
         File file = new File(World.getActiveInstance().getCityModel().getGmlPath());
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(file);
         doc.getDocumentElement().normalize();
-        List<Node> nodeList = new ArrayList<>();
-        XmlUtil.recursiveFindNodeByAttribute(doc, nodeList, TagName.GML_ID);
 
-        boolean hasErrorNull = false;
-        for (Node node : nodeList) {
-            String id = node.getAttributes().getNamedItem(TagName.GML_ID).getNodeValue();
-            if (ObjectUtils.isEmpty(id)) {
-                if (!hasErrorNull) {
-                    hasErrorNull = true;
-                    messages.add(new ValidationResultMessage(ValidationResultMessageType.Error,
-                            String.format("Exists GmlId null\n")));
-                }
-                continue;
-            }
+        // get all tag in document
+        List<Node> allTags = new ArrayList<>();
+        XmlUtil.recursiveFindNodeByAttribute(doc, allTags, TagName.GML_ID);
+        List<ValidationResultMessage> messages = new ArrayList<>();
+        messages.add(new ValidationResultMessage(ValidationResultMessageType.Info, "gml:idの完全性を検証中..."));
 
-            if (gmlIDs.contains(id)) {
-                messages.add(new ValidationResultMessage(ValidationResultMessageType.Error,
-                    String.format("%sは重複して使用されています。\n", id)));
-            }
-            gmlIDs.add(id);
+        List<String> allID = allTags.stream().map(e -> ((Element) e).getAttribute(TagName.GML_ID)).collect(Collectors.toList());
+        if (allID.contains("")) {
+            messages.add(new ValidationResultMessage(ValidationResultMessageType.Error, "Exists GmlId null\\n"));
         }
-
+        Map<String, Integer> elementCountMap = new HashMap<>();
+        // Count the number of occurrences of each element
+        for (String gmlID : allID) {
+            elementCountMap.put(gmlID, elementCountMap.getOrDefault(gmlID, 0) + 1);
+        }
+        for (Map.Entry<String, Integer> entry : elementCountMap.entrySet()) {
+            String gmlID = entry.getKey();
+            if (entry.getValue() > 1) {
+                messages.add(new ValidationResultMessage(ValidationResultMessageType.Error, String.format("%sは重複して使用されています。\n", gmlID)));
+            }
+        }
         return messages;
     }
 }
