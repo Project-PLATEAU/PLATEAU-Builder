694253384404abda9412242aa8fb2d16875120b2
HirokazuOnishi <kumakuma4w@gmail.com>
2024-02-07 19:28:16 +0900

    フッターにGeoReference情報追加

---
+++ src/main/java/org/plateau/citygmleditor/fxml/InfoFooterController.java
@@ -0,0 +1,12 @@
+package org.plateau.citygmleditor.fxml;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class InfoFooterController {
+    @FXML
+    private Label epsgCodeValue;
+    @FXML
+    private Label offsetValue;
+    
+}
