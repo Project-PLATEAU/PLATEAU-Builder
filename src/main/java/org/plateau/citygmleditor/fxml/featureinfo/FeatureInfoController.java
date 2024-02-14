ddd2a9316e97ffbd143fe91eb6e277ee89f46288
Kazumasa Sakiyama <kazsakinotwen@gmail.com>
2024-02-14 12:50:34 +0900

    LOD選択UI追加 (#205)

--- src/main/java/org/plateau/citygmleditor/fxml/featureinfo/FeatureInfoController.java
+++ src/main/java/org/plateau/citygmleditor/fxml/featureinfo/FeatureInfoController.java
@@ -2,21 +2,28 @@ package org.plateau.citygmleditor.fxml.featureinfo;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.plateau.citygmleditor.citygmleditor.CityGMLEditorApp;
 import org.plateau.citygmleditor.fxml.UIConstants;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class FeatureInfoController implements Initializable {
     @FXML
+    private ToggleButton lod1Toggle;
+    @FXML
+    private ToggleButton lod2Toggle;
+    @FXML
+    private ToggleButton lod3Toggle;
+    @FXML
     private Text featureIDText;
-
     @FXML
     private Text featureTypeText;
-
     @FXML
     private VBox featureInfoContainer;
 
@@ -26,6 +33,19 @@ public class FeatureInfoController implements Initializable {
 
         CityGMLEditorApp.getFeatureSellection().getActiveFeatureProperty()
                 .addListener((observable, oldFeature, feature) -> {
+                    var toggles = new ArrayList<ToggleButton>();
+                    toggles.add(lod1Toggle);
+                    toggles.add(lod2Toggle);
+                    toggles.add(lod3Toggle);
+
+                    for (int lod = 1; lod <= 3; ++lod) {
+                        var toggle = toggles.get(lod - 1);
+                        if (oldFeature == null || oldFeature.getSolid(lod) == null)
+                            continue;
+
+                        toggle.selectedProperty().unbindBidirectional(((Node) oldFeature.getSolid(lod)).visibleProperty());
+                    }
+
                     if (feature == null) {
                         featureInfoContainer.setVisible(false);
                         return;
@@ -35,6 +55,19 @@ public class FeatureInfoController implements Initializable {
 
                     featureIDText.setText("地物ID：" + feature.getGMLObject().getId());
                     featureTypeText.setText("地物型：" + UIConstants.buildingTypeDescription(feature.getGMLObject().getCityGMLClass()));
+
+                    for (int lod = 1; lod <= 3; ++lod) {
+                        var toggle = toggles.get(lod - 1);
+                        if (feature.getSolid(lod) == null) {
+                            toggle.setVisible(false);
+                            continue;
+                        }
+
+                        toggle.setVisible(true);
+                        if (oldFeature != null && oldFeature.getSolid(lod) != null)
+                            toggle.selectedProperty().unbindBidirectional(((Node)oldFeature.getSolid(lod)).visibleProperty());
+                        toggle.selectedProperty().bindBidirectional(((Node)feature.getSolid(lod)).visibleProperty());
+                    }
                 });
     }
 }
